12e1164e10ced7b605df5819614dfaf13e4262a3
Kaushal2701 <kaushalswami7178@gmail.com>
2021-11-14 13:45:00 +0530

    main

---
+++ src/app/space-x/space-x.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-x',
+  templateUrl: './space-x.component.html',
+  styleUrls: ['./space-x.component.css']
+})
+export class SpaceXComponent implements OnInit {
+  data: any = []
+  link1 = 'https://api.spacexdata.com/v3/missions'
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.link1).subscribe((responseData: any) => {
+      this.data = responseData;
+    })
+  }
+
+}
